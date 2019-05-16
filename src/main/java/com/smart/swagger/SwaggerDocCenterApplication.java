a706833dad3d231dcc923d77f5db602a2448d84d
guwenchang <1379864599@qq.com>
2019-05-16 13:08:35 +0800

    init

---
+++ src/main/java/com/smart/swagger/SwaggerDocCenterApplication.java
@@ -0,0 +1,18 @@
+package com.smart.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 基于swagger的文档托管中心
+ * @author guwenchang
+ * @date 2019-05-16
+ */
+@SpringBootApplication
+public class SwaggerDocCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerDocCenterApplication.class, args);
+    }
+
+}
